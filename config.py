756fad0ada30594b9d3245b53f81c4f11d3d2f24
Vladimir Dobrikov <hedin.mail@gmail.com>
2022-08-21 00:27:52 +0200

    First working version

---
+++ config.py
@@ -0,0 +1,5 @@
+motion_sensor_pin = 4
+data_folder = 'data'
+delete_pictures_older_than_seconds = 60 * 60 * 24 * 30  # 1 month
+telepot_token = 'placeholder'
+telepot_chat_id = 'placeholder'
--- motion.py
+++ motion.py
@@ -1,39 +1,86 @@
-from time import sleep
-
-# import RPi.GPIO as GPIO
+import logging
+import os
+import time
 import telepot
+import config
 from gpiozero import MotionSensor
+from logging.config import fileConfig
+from picamera2 import Picamera2
 
-from log import get_logger
 
-log = get_logger()
-PIR_PIN = 4
+def create_camera():
+    cam = Picamera2()
+    cam.start()
+    time.sleep(2)
+    capture_config = cam.create_still_configuration()
+    cam.switch_mode(capture_config)
+    return cam
 
-pir = MotionSensor(PIR_PIN)
 
+def create_folder_if_not_exists(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+
+def send_picture(file):
+    try:
+        log.debug("Sending picture")
+        bot.sendPhoto(config.telepot_chat_id, open(file, 'rb'))
+    except Exception as e:
+        log.error("Error sending picture: {}".format(e))
+
+
+def send_message(message):
+    try:
+        log.debug("Sending message: {}".format(message))
+        bot.sendMessage(config.telepot_chat_id, message)
+    except Exception as e:
+        log.error("Error sending message: {}".format(e))
+
+
+def generate_date_time_filename(prefix):
+    return '{}_{}_{}_{}_{}.jpg'.format(prefix, time.strftime('%Y'), time.strftime('%m'), time.strftime('%d'),
+                                       time.strftime('%H'))
+
+
+def take_picture():
+    try:
+        log.info("Taking picture")
+        file = generate_date_time_filename("./{}/motion".format(config.data_folder))
+        camera.capture_file(file)
+        return file
+    except Exception as e:
+        log.error("Error taking picture: {}".format(e))
+        return None
+
+
+def delete_files_older_than(seconds, directory):
+    try:
+        for file in os.listdir(directory):
+            if os.stat(os.path.join(directory, file)).st_mtime < time.time() - seconds:
+                os.remove(os.path.join(directory, file))
+    except Exception as e:
+        log.error("Error deleting files: {}".format(e))
+
+
+fileConfig('logging.ini')
+log = logging.getLogger('Main')
+
+camera = create_camera()
+pir = MotionSensor(config.motion_sensor_pin)
+bot = telepot.Bot(config.telepot_token)
 log.info("Motion sensor started")
+
+create_folder_if_not_exists(config.data_folder)
 while True:
     pir.wait_for_motion()
-    log.info("You moved")
-    pir.wait_for_no_motion()
-
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setup(PIR_PIN, GPIO.IN)
-#
-#
-# def motion_callback():
-#     print("Motion detected!")
-#
-#
-# def motion_detected():
-#     return GPIO.input(PIR_PIN)
-#
-#
-# while True:
-#     if motion_detected():
-#         motion_callback()
-#     else:
-#         pass
-#     sleep(0.1)
-
-# GPIO.add_event_detect(PIR_PIN, GPIO.RISING, callback=motion_callback)
+    log.info("Motion detected")
+    send_message("Motion detected")
+    while pir.motion_detected:
+        filename = take_picture()
+        if filename:
+            send_picture(filename)
+        time.sleep(1)
+    log.info("No motion")
+    send_message("No motion")
+    delete_files_older_than(config.delete_pictures_older_than_seconds, config.data_folder)
